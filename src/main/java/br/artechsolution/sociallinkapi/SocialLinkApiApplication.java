19b3c5a52c8ba7a2df32542c18677ef8b5a2c1ff
Alison Rodrigues <alison.si1302@gmail.com>
2023-03-24 11:13:34 -0300

    first commit

---
+++ src/main/java/br/artechsolution/sociallinkapi/SocialLinkApiApplication.java
@@ -0,0 +1,13 @@
+package br.artechsolution.sociallinkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLinkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialLinkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/artechsolution/sociallinkapi/SocialLinkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.artechsolution.sociallinkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLinkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
